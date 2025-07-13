26e4c6b084674e919e1694cf0f0e9a78bd397428
Robert Vitonsky <robert@vitonsky.net>
2025-07-13 21:17:46 +0200

    chore: update demo

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,9 +1,16 @@
-import { LanguageInfoScheme } from '../types/languages';
+import { CountrySummaryScheme } from '../types/countries';
+import { LanguageSummaryScheme } from '../types/languages';
 import { readFile } from 'fs/promises';
-import { LANGUAGES_DB_PATH } from './const';
+import { COUNTRIES_DB_PATH, LANGUAGES_DB_PATH } from './const';
 
 export const getLanguagesList = async () => {
 	const rawJson = await readFile(LANGUAGES_DB_PATH, { encoding: 'utf8' });
 
-	return LanguageInfoScheme.array().parse(JSON.parse(rawJson));
+	return LanguageSummaryScheme.array().parse(JSON.parse(rawJson));
+};
+
+export const getCountriesList = async () => {
+	const rawJson = await readFile(COUNTRIES_DB_PATH, { encoding: 'utf8' });
+
+	return CountrySummaryScheme.array().parse(JSON.parse(rawJson));
 };
--- src/demo.ts
+++ src/demo.ts
@@ -1,21 +1,40 @@
-import { getLanguagesList } from './database';
+import { getCountriesList, getLanguagesList } from './database';
 
-getLanguagesList()
-	.then((languages) => {
+Promise.all([getLanguagesList(), getCountriesList()])
+
+	.then(([languages, countries]) => {
+		const languageNames = new Intl.DisplayNames(['en'], { type: 'language' });
+		const countryNames = new Intl.DisplayNames(['en'], { type: 'region' });
+
 		console.log(`Top languages:`);
 
 		let order = 0;
-		for (const language of languages) {
+		for (const language of languages.slice(0, 20)) {
 			const { codes, speakers } = language;
 			const code = codes.iso639_1 ?? codes.iso639_3;
 			if (!code) continue;
 
-			const languageNames = new Intl.DisplayNames(['en'], { type: 'language' });
 			const displayName = languageNames.of(code);
 			if (displayName === code) continue;
 
+			const { iso639_1: shortLangCode } = codes;
+			const countriesList = shortLangCode
+				? countries
+						.filter(({ languages }) => languages.includes(shortLangCode))
+						.sort((a, b) => b.population - a.population)
+						.map(({ code }) => countryNames.of(code))
+						.slice(0, 10)
+						.join(', ')
+				: '';
+
 			console.log(
-				`#${order++} ${languageNames.of(code)} (${[codes.iso639_1, codes.iso639_3].filter(Boolean).join(',')}) - total speakers: ${speakers.total}`,
+				[
+					`#${++order} ${languageNames.of(code)} (${[codes.iso639_1, codes.iso639_3].filter(Boolean).join(',')})`,
+					`- Total speakers: ${speakers.total}`,
+					countriesList && `- Top 10 countries where used: ${countriesList}`,
+				]
+					.filter(Boolean)
+					.join('\n'),
 			);
 		}
 	})
